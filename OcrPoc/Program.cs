2891d60db05635d086c037610a9a44798d01bce5
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-06-09 15:35:57 +0300

    Add solution to source control

---
+++ OcrPoc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Tesseract;
+
+namespace OcrPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var tesseract = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default))
+            {
+                foreach (var file in Directory.EnumerateFiles("Samples"))
+                {
+                    using (var img = Pix.LoadFromFile(file))
+                    {
+                        using (var page = tesseract.Process(img))
+                        {
+                            var result = page.GetText();
+                            File.WriteAllText(string.Format(@"{0}.txt", file), result);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Done @ {0}", DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
