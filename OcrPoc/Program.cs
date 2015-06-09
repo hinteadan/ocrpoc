fa84813606a8b9819a27c9ecfd300b46a8563795
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-06-09 17:26:20 +0300

    Play around with samples

--- OcrPoc/Program.cs
+++ OcrPoc/Program.cs
@@ -8,16 +8,27 @@ namespace OcrPoc
     {
         static void Main(string[] args)
         {
+            if (!Directory.Exists(@"Samples\..\Processed"))
+            {
+                Directory.CreateDirectory(@"Samples\..\Processed");
+            }
+
+            if (!Directory.Exists(@"Samples\..\Results"))
+            {
+                Directory.CreateDirectory(@"Samples\..\Results");
+            }
+
             using (var tesseract = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default))
             {
                 foreach (var file in Directory.EnumerateFiles("Samples"))
                 {
+                    var fileInfo = new FileInfo(file);
                     using (var img = Pix.LoadFromFile(file))
                     {
-                        using (var page = tesseract.Process(img))
+                        using (var page = tesseract.Process(PreProcessAndStoreImage(img, PreProcessImageWithOtsuAdaptive, string.Format(@"{0}\..\Processed\{1}", fileInfo.DirectoryName, fileInfo.Name))))
                         {
                             var result = page.GetText();
-                            File.WriteAllText(string.Format(@"{0}.txt", file), result);
+                            File.WriteAllText(string.Format(@"{0}\..\Results\{1}.txt", fileInfo.DirectoryName, fileInfo.Name), result);
                         }
                     }
                 }
@@ -25,6 +36,29 @@ namespace OcrPoc
 
             Console.WriteLine("Done @ {0}", DateTime.Now);
             Console.ReadLine();
+        }
+
+        private static Pix PreProcessImageWithOtsuAdaptive(Pix img)
+        {
+            try
+            {
+                return img.ConvertRGBToGray(50, 50, 50).BinarizeOtsuAdaptiveThreshold(1, 1, 0, 0, 0.0f);
+            }
+            catch(Exception)
+            {
+                return img;
+            }
+        }
+
+        private static Pix PreProcessAndStoreImage(Pix source, Func<Pix, Pix> processor, string filePath)
+        {
+            var img = processor(source);
+            try
+            {
+                img.Save(filePath);
+            }
+            catch (Exception) { }
+            return img;
         }
     }
 }
